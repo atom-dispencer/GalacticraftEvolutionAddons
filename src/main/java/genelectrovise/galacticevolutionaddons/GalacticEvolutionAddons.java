37be7475c00771029889e8e121cc84eab0c26cb2
GenElectrovise <46679475+GenElectrovise@users.noreply.github.com>
2023-02-10 17:31:22 +0000

    Created recipes for barbed wire and barbed wire fences.

--- src/main/java/genelectrovise/galacticevolutionaddons/GalacticEvolutionAddons.java
+++ src/main/java/genelectrovise/galacticevolutionaddons/GalacticEvolutionAddons.java
@@ -1,17 +1,17 @@
 /**
  * Galactic Evolution Addons - Tweaks and compatibility improvements for the Galactic Evolution mod-pack for Minecraft.
  * Copyright (C) 2023  GenElectrovise
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -25,9 +25,10 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Arrays;
+
 @Mod(modid = GalacticEvolutionAddons.MODID, name = GalacticEvolutionAddons.NAME, version = GalacticEvolutionAddons.VERSION)
-public class GalacticEvolutionAddons
-{
+public class GalacticEvolutionAddons {
     public static final String MODID = "galacticevolutionaddons";
     public static final String NAME = "Galactic Evolution Addons";
     public static final String VERSION = "1.0";
@@ -35,15 +36,13 @@ public class GalacticEvolutionAddons
     private static Logger LOGGER;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
         LOGGER = event.getModLog();
         LOGGER.info(NAME + " - Pre-initialisation");
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
         LOGGER.info(NAME + " - Initialisation");
 
         LOGGER.info("Adding Galactic Evolution Addons OpenComputers drivers...");
@@ -59,5 +58,7 @@ public class GalacticEvolutionAddons
         Driver.add(new IC2TransformerDriver());
         Driver.add(new IC2WaterKineticDriver());
         Driver.add(new IC2WindKineticDriver());
+
+        // ForgeRegistries.RECIPES.register(new BarbedWireRecipe());
     }
 }
